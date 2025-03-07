bdb136b8bab992b87d50a784d410a7f7a02896f7
tbprayse <obipraisechinedu05@gmail.com>
2025-03-07 12:57:14 +0100

    ttoil updated

--- src/component/productinfo/productinfo.jsx
+++ src/component/productinfo/productinfo.jsx
@@ -2,28 +2,15 @@ import "./productinfo.css";
 
 const ProductInfo = ({ pimg, heading, para }) => {
   return (
-    <>
-      <div className="imgproduct">
-        <div>
-          <img src={pimg} alt="" />
-        </div>
-        <div className="productinfo">
-          <h5>{heading}</h5>
-          <p>{para}</p>
-        </div>
+    <div className="imgproduct">
+      <div>
+        <img src={pimg} alt="" />
       </div>
-      {/* <div className="productdetail">
-        {/* <div className="imgswap">
-        <div className="productinfo">
-          <h5>{heading2}</h5>
-          <p>{para2}</p>
-        </div>
-        <div>
-          <img src={pimg2} alt="" />
-        </div>
+      <div className="productinfo">
+        <h5>{heading}</h5>
+        <p>{para}</p>
       </div>
-      </div> */}
-    </>
+    </div>
   );
 };
 
